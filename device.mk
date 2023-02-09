54247d8dce37d88faeee4001babcb19607ba123b
Vimal Pandey <vpdse009@gmail.com>
2023-02-09 21:17:38 +0530

    dandelion: Add IMS Jars
    Essential for VoLTE services

--- device.mk
+++ device.mk
@@ -107,6 +107,19 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.dandelion
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+PRODUCT_PACKAGES += \
+    ImsServiceBase
+
 # KPOC
 PRODUCT_PACKAGES += \
     libsuspend \
